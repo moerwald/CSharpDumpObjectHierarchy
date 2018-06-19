3b144c89ea26fa7a7c89dfe8a9796f7cde62bddc
andreas mewald <andreas.mewald@pdts.at>
2018-06-19 10:18:56 +0200

    Add project files.

---
+++ DumpInheritanceHierarchy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DumpInheritanceHierarchy
+{
+
+    class A
+    {
+    }
+
+    class B:A
+    {
+    }
+
+    class C:B
+    {
+    }
+
+    class D:C
+    {
+
+    }
+    public static class HierarchyDumper
+    {
+        public static IEnumerable<Type> GetBaseTypes(this Type type) => type.BaseType == null ? new Type[0] : Enumerable.Repeat(type.BaseType, 1).Concat(type.BaseType.GetBaseTypes());
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new D();
+            var result = d.GetType().GetBaseTypes().ToList();
+            Console.WriteLine($"{string.Join("->", result.Select(t => t.Name))}");
+        }
+    }
+}
