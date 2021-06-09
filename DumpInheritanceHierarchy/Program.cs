40f7d747cb22be702d5ac1c41e25c11df2f6f1a4
moerwald <andreas.mewald@gmail.com>
2021-06-09 14:15:37 +0200

    formating

--- DumpInheritanceHierarchy/Program.cs
+++ DumpInheritanceHierarchy/Program.cs
@@ -4,26 +4,20 @@ using System.Linq;
 
 namespace DumpInheritanceHierarchy
 {
+    class A { }
 
-    class A
-    {
-    }
+    class B:A { }
 
-    class B:A
-    {
-    }
+    class C:B { }
 
-    class C:B
-    {
-    }
-
-    class D:C
-    {
-
-    }
+    class D:C { }
+    
     public static class HierarchyDumper
     {
-        public static IEnumerable<Type> GetBaseTypes(this Type type) => type.BaseType == null ? new Type[0] : Enumerable.Repeat(type.BaseType, 1).Concat(type.BaseType.GetBaseTypes());
+        public static IEnumerable<Type> GetBaseTypes(this Type type) => 
+            type.BaseType == null ? new Type[0] 
+                                  : Enumerable.Repeat(type.BaseType, 1)
+                                              .Concat(type.BaseType.GetBaseTypes() /* recursive call */);
     }
 
     class Program
@@ -34,7 +28,6 @@ namespace DumpInheritanceHierarchy
             var result = d.GetType().GetBaseTypes().ToList();
             // Prints: "C->B->A->Object"
             Console.WriteLine($"{string.Join("->", result.Select(t => t.Name))}");
-
         }
     }
 }
